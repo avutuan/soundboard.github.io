ebb0f7a67938bf2b95fbf9bf15a920fb273d09fa
Tuan Vu <business.tuanvu@gmail.com>
2024-02-02 10:25:09 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+function vineboom(){
+    var snd = new Audio('sounds/vineboom.wav')
+    snd.play()
+}
+function bruh(){
+    var snd = new Audio('sounds/bruh.mp3')
+    snd.play()
+}
+function ohhellnah(){
+    var snd = new Audio('sounds/ohhellnah.mp3')
+    snd.play()
+}
+function a_ohhellnah(){
+    var snd = new Audio('sounds/a_ohhellnah.wav')
+    snd.play()
+}
+function dreams(){
+    var snd = new Audio('sounds/dreams.mp3')
+    snd.volume=0.8
+    snd.play()
+}
+
